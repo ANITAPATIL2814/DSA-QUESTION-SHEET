eb49e8752c05c892d3dd2944d50da0776aafb8b1
anitapatil2814 <anitapatil2814@gmail.com>
2024-08-20 19:12:30 +0530

    first commit

--- DSA/BINARY_TREE/univalued.java
+++ DSA/BINARY_TREE/univalued.java
@@ -1,45 +1,48 @@
 package DSA.BINARY_TREE;
 
-
 public class univalued {
-    static class Node{
-        int data;
-        Node left;
-        Node right;
-        Node(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
+    
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
-    public class TreeNode {
-             int val;
-             TreeNode left;
-             TreeNode right;
-             TreeNode() {}
-             TreeNode(int val) { this.val = val; }
-             TreeNode(int val, TreeNode left, TreeNode right) {
-                 this.val = val;
-                 this.left = left;
-                 this.right = right;
-             }
-         }
+
     public boolean isUnivalTree(TreeNode root) {
-        if(root ==null) return true;
-        if(root.left!= null && root.val !=root.left.val) return false;
-        if(root.right!= null && root.val !=root.right.val) return false;
-        boolean leftcall=isUnivalTree(root.left);
-        boolean rightcall=isUnivalTree(root.right);
-        return leftcall && rightcall ;
+        if (root == null) return true;
+        if (root.left != null && root.val != root.left.val) return false;
+        if (root.right != null && root.val != root.right.val) return false;
+
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
     }
+
     public static void main(String[] args) {
-        Node root=new Node(1);
-        root.left=new Node(1);
-        root.right=new Node(1);
-        root.left.left=new Node(1);
-        root.left.right=new Node(1);
-        root.right.left=new Node(1);
-        root.right.right=new Node(1);
-    
+        univalued tree = new univalued();
+        
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(1);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(1);
+
+        if (tree.isUnivalTree(root)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
