a9963d7d0b855516f04e1ad45498c4f70bfe338f
anitapatil2814 <anitapatil2814@gmail.com>
2024-08-27 10:16:25 +0530

    first commit

--- DSA/HEAPS/pq_objects.java
+++ DSA/HEAPS/pq_objects.java
@@ -4,27 +4,26 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class pq_objects {
-    static class A implements Comparable<A> {
+    static class Students implements Comparable<Students> {
         String name;
         int rank;
-        Public A(String name, int rank){
+        public Students(String name, int rank){
             this.name=name;
             this.rank=rank;
-            return null;
         }
         @Override
-        public int compareTo(A o) {
-            return o.rank- o.rank;
-            }
-        
+        public int compareTo(Students o) {
+            return this.rank- o.rank;
+            }  
     }
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(3); //O(logn)
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+        PriorityQueue<Students> pq=new PriorityQueue<>();
+       
+        pq.add(new Students("anita",4)); //O(logn)
+        pq.add(new Students("vasu",5));
+        pq.add(new Students("utsav",7));
+        pq.add(new Students("diapk",9));
         while (!pq.isEmpty()) {
             System.out.println(pq.peek());//O(1)
             pq.remove();//O(logn)
